2bc1db0048ee4f42c97404aa395297b2f692021e
ourat711 <ourat0830@gmail.com>
2022-09-27 22:15:29 +0900

    20

---
+++ chapter3/21.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+import gzip
+import json
+fileName = "jawiki-country.json.gz"
+pickUp = "[[Category:"
+
+def wikiDataList():
+    wikiDataList = []
+    with gzip.open(fileName, mode='rt') as dataFile:
+        for line in dataFile:
+            wikiDataList.append(json.loads(line))
+    return wikiDataList
+
+for list in wikiDataList():
+    lists = list['text'].splitlines()
+
+    pickUpList = [i for i in lists if pickUp in i] 
+
+    if bool(pickUpList):
+        print(pickUpList)
